33bf988614f4f5df7ea831ce8063647d2cd77e0b
huntbao <gzooler@gmail.com>
2015-12-20 21:55:52 +0800

    add test_pro

--- js/page.js
+++ js/page.js
@@ -1,3 +1,21 @@
 ﻿(function () {
+    'use strict'
+    let $ = (selector) => {
+        return document.querySelectorAll(selector)
+    }
 
+    class Page {
+        constructor() {
+            let todo = $('.new-todo')[0]
+            todo.value = 'test text'
+            var event = new KeyboardEvent('keydown', {
+                'view': window
+            })
+            todo.dispatchEvent(event)
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        new Page()
+    });
 })()
