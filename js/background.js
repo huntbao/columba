ff2de2ed80d21e3e7593ecd6e6632e508a438c13
hzbaoyongming <hzbaoyongming@corp.netease.com>
2015-12-21 20:41:52 +0800

    exec code in background

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
-﻿(function ($) {
+﻿(function () {
+    /*global chrome */
+    // see https://developer.chrome.com/extensions/content_scripts
+    'use strict'
+    let code = "alert(1)"
 
+    chrome.browserAction.onClicked.addListener(function(tab) {
+        chrome.tabs.executeScript({
+            code: code
+        })
+    })
 })()
