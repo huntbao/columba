fb0469ce2262be9830a2c0448d1b7a0785b021e5
HuntBao <gzooler@gmail.com>
2015-12-22 16:25:56 +0800

    options page add tests func

--- js/background.js
+++ js/background.js
@@ -1,26 +1,30 @@
 ﻿//author @huntbao
-(function () {
-    /*global chrome */
-    // see https://developer.chrome.com/extensions/content_scripts
-    'use strict'
-    let code = "" +
-        "describe('Todo test', function () {" +
-        "it('Add todo', function () {" +
-        "var newTodo = 'New todo test';" +
-        "var todo = $('.new-todo')[0];" +
-        "todo.value = newTodo;" +
-        "var event = new Event('change');" +
-        "todo.dispatchEvent(event);" +
-        "var addResult = $('.todo-list > li label')[0];" +
-        "expect(addResult.textContent).toBe(newTodo);" +
-        "})" +
-        "});"
-    chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.executeScript({
-            code: code
+/*global chrome */
+// see https://developer.chrome.com/extensions/content_scripts
+
+'use strict'
+
+const SAVED_TESTS_STR = 'saved_tests'
+
+class BGPage {
+
+    constructor() {
+        this.addEvent()
+    }
+
+    addEvent() {
+        chrome.browserAction.onClicked.addListener(function(tab) {
+            StorageArea.get(SAVED_TESTS_STR, (result) => {
+                let code = result[SAVED_TESTS_STR]
+                chrome.tabs.executeScript({
+                    code: code
+                })
+                chrome.tabs.executeScript({
+                    code: 'jasmineRun()'
+                })
+            })
         })
-        chrome.tabs.executeScript({
-            code: 'jasmineRun()'
-        })
-    })
-})()
+    }
+}
+
+new BGPage()
---
+++ js/storagearea.js
@@ -0,0 +1,34 @@
+//author @huntbao
+
+/*global window */
+'use strict'
+
+class StorageArea {
+
+    static get(keys, callback) {
+        chrome.storage.local.get(keys, function (result) {
+            callback(result)
+        })
+    }
+
+    static set(data, callback) {
+        chrome.storage.local.set(data, function () {
+            callback && callback()
+        })
+    }
+
+    static remove(keys, callback) {
+        chrome.storage.local.remove(keys, () => {
+            callback && callback()
+        })
+    }
+
+    static clear(callback) {
+        chrome.storage.local.clear(() => {
+            callback && callback()
+        })
+    }
+
+}
+
+window.StorageArea = StorageArea
