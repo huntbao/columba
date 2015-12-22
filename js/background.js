643a083fa64c336e9240587e54d6397d0050e134
HuntBao <gzooler@gmail.com>
2015-12-22 10:27:40 +0800

    add jasmine

--- js/background.js
+++ js/background.js
@@ -2,11 +2,24 @@
     /*global chrome */
     // see https://developer.chrome.com/extensions/content_scripts
     'use strict'
-    let code = "alert(1)"
-
+    let code = "" +
+        "describe('Todo test', function () {" +
+        "it('Add todo', function () {" +
+        "var newTodo = 'New todo test';" +
+        "var todo = $('.new-todo')[0];" +
+        "todo.value = newTodo;" +
+        "var event = new Event('change');" +
+        "todo.dispatchEvent(event);" +
+        "var addResult = $('.todo-list > li label')[0];" +
+        "expect(addResult.textContent).toBe(newTodo);" +
+        "})" +
+        "});"
     chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.executeScript({
             code: code
+        })
+        chrome.tabs.executeScript({
+            code: 'jasmineRun()'
         })
     })
 })()
